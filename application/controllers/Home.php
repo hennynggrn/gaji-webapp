8a6817c13fb005052daab4a0c075fa324700da78
Widiya Sasih <widiyasasih@gmail.com>
2020-12-31 03:10:45 +0700

    Debugging calculation

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -77,31 +77,28 @@ class Home extends CI_Controller {
 				$honor_val = 0;
 			}
 
+			$pasangan = 0;
+			$anak_pertama = 0;
+			$anak_kedua = 0;
 			if ($pegawai['klg_hidup'] != NULL) {
-				$pasangan = 0;
-				$anak_pertama = 0;
-				$anak_kedua = 0;
-				for ($i=0; $i < $pegawai['klg_hidup']; $i++) { 
-					if (in_array('1', $pegawai['status_klg'])) {
-						$pasangan = 1;
-					} else if (in_array('2', $pegawai['status_klg'])) {
-						$anak_pertama = 1;
-					} else {
-						$anak_kedua = 1;
-					}
+				if (in_array('1', $pegawai['status_klg'])){
+					$pasangan = 1;
+				} 
+				if (in_array('2', $pegawai['status_klg'])) {
+					$anak_pertama = 1;
+				} 
+				if (in_array('3', $pegawai['status_klg'])) {
+					$anak_kedua = 1;
 				}
-			} else {
-				$pasangan = 0;
-				$anak_pertama = 0;
-				$anak_kedua = 0;
 			}
 			$keluarga_val = ($pegawai['honor']*$pasangan*$data['tunjangan']['klg_psg'])+($pegawai['honor']*$anak_pertama*$data['tunjangan']['klg_anak'])+($pegawai['honor']*$anak_kedua*$data['tunjangan']['klg_anak']);
 			$tunjangan_val = $data['tunjangan']['beras']+$data['tunjangan']['jamsostek']+$keluarga_val+$pegawai['jabatan']+$pegawai['nominal_mk'];
 
+			($pegawai['status_pegawai'] != 'P') ? $aisiyah_val = 0 : $aisiyah_val = $data['potongan']['aisiyah'];
 			(!empty($pegawai['nominal_kop'])) ? $pjm_kop = $pegawai['nominal_kop'] : $pjm_kop = 0;
 			(!empty($pegawai['nominal_bank'])) ? $pjm_bank = $pegawai['nominal_bank'] : $pjm_bank = 0;
 			$pinjaman = $pjm_kop+$pjm_bank;
-			$potongan_val = $data['potongan']['sosial']+$data['potongan']['infaq']+$data['potongan']['jsr']+$data['potongan']['aisiyah']+$data['potongan']['jamsostek']+$pinjaman;
+			$potongan_val = $data['potongan']['sosial']+$data['potongan']['infaq']+$data['potongan']['jsr']+$aisiyah_val+$data['potongan']['jamsostek']+$pinjaman;
 			$gaji = $honor_val+$tunjangan_val-$potongan_val;
 
 			$data_output[$key]['honor_val'] = $honor_val;
