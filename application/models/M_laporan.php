788c6299c1c37d2242a94dc7291c72045fcb402e
Widiya Sasih <widiyasasih@gmail.com>
2021-01-16 15:44:55 +0700

    update modal laporan

--- application/models/M_laporan.php
+++ application/models/M_laporan.php
@@ -12,13 +12,15 @@ class M_laporan extends CI_Model
 			$this->db->join('pegawai p', 'p.id_pegawai = bg.id_pegawai', 'LEFT');
 			$data = $this->db->get_where('b_gaji bg', array('month(bg.created_at)' => $month, 'year(bg.created_at)' => $year));
 		} else {
-			$data = $this->db->query("
+			$data = $this->db->query('
 				SELECT *, count(id_gaji) jml_gaji
 				FROM b_gaji
 				GROUP BY 
-					CAST(YEAR(created_at) AS VARCHAR(4)) + '-' + right('00' + CAST(MONTH(created_at) AS VARCHAR(2)), 2)
-				ORDER BY created_at DESC
-			");
+					CAST(YEAR(created_at) AS VARCHAR(4)) + "-" + CAST(MONTH(created_at) AS VARCHAR(2))
+				ORDER BY 
+					created_at DESC
+			');
+			// CAST(YEAR(created_at) AS VARCHAR(4)) + "-" + right("00" + CAST(MONTH(created_at) AS VARCHAR(2)), 2)
 		}
 		return $data;
 	}
